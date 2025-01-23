18ace896fba4b51a9832d1aeccdc68689c5be1fe
Felipe Pinheiro <flpinheiro@gmail.com>
2025-01-23 20:22:07 -0300

    add autorization

--- RestFull.BFFApi/Program.cs
+++ RestFull.BFFApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,47 @@ builder.Services.AddControllers();
 
 builder.Services.AddOcelotService(builder.Configuration);
 builder.Services.AddSwaggerService(builder.Configuration);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(cookie =>
+{
+    //Sets the cookie name and manage, so the cookie is invalidated.  
+    cookie.Cookie.Name = "keycloak.cookie";
+    cookie.Cookie.MaxAge = TimeSpan.FromMinutes(60);
+    cookie.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    cookie.SlidingExpiration = true;
+})
+.AddOpenIdConnect(options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+    options.Authority = builder.Configuration.GetValue<string>("Keycloak:RealmUrl");
+    options.ClientId = builder.Configuration.GetValue<string>("Keycloak:ClientId");
+    options.ClientSecret = builder.Configuration.GetValue<string>("Keycloak:ClientSecret");
+    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    // Disable HTTPS requirement for metadata in development
+    options.RequireHttpsMetadata = false;
+    options.CorrelationCookie.SameSite = SameSiteMode.Lax;
+    options.NonceCookie.SameSite = SameSiteMode.Lax;
+    //Save the token  
+    options.SaveTokens = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("email");
+    options.Scope.Add("phone");
+    options.Scope.Add("profile");
+    // options.NonceCookie.SameSite = SameSiteMode.None;
+    // options.CorrelationCookie.SameSite = SameSiteMode.None;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "name",
+        RoleClaimType = "https://schemas.scopic.com/roles"
+    };
+});
 
 var app = builder.Build();
 
--- RestFullApi.Web/Program.cs
+++ RestFullApi.Web/Program.cs
@@ -2,6 +2,10 @@ using MudBlazor.Services;
 using RestFullApi.Web.Components;
 using RestFullApi.Web.Services;
 using Refit;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +22,46 @@ builder.Services
     .AddRefitClient<IBFFApiService>()
     .ConfigureHttpClient(c => c.BaseAddress = new Uri("https+http://bffApiService"));
 
-//builder.Services.AddHttpClient<WeatherApiClient>(client =>
-//    {
-//        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-//        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-//        client.BaseAddress = new("https+http://bff");
-//    });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(cookie =>
+{
+    //Sets the cookie name and manage, so the cookie is invalidated.  
+    cookie.Cookie.Name = "keycloak.cookie";
+    cookie.Cookie.MaxAge = TimeSpan.FromMinutes(60);
+    cookie.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    cookie.SlidingExpiration = true;
+})
+.AddOpenIdConnect(options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+    options.Authority = builder.Configuration.GetValue<string>("Keycloak:RealmUrl");
+    options.ClientId = builder.Configuration.GetValue<string>("Keycloak:ClientId");
+    options.ClientSecret = builder.Configuration.GetValue<string>("Keycloak:ClientSecret");
+    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    // Disable HTTPS requirement for metadata in development
+    options.RequireHttpsMetadata = false;
+    options.CorrelationCookie.SameSite = SameSiteMode.Lax;
+    options.NonceCookie.SameSite = SameSiteMode.Lax;
+    //Save the token  
+    options.SaveTokens = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("email");
+    options.Scope.Add("phone");
+    options.Scope.Add("profile");
+    // options.NonceCookie.SameSite = SameSiteMode.None;
+    // options.CorrelationCookie.SameSite = SameSiteMode.None;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "name",
+        RoleClaimType = "https://schemas.scopic.com/roles"
+    };
+});
 
 var app = builder.Build();
 
@@ -41,6 +79,9 @@ app.UseAntiforgery();
 app.UseOutputCache();
 
 app.MapStaticAssets();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
